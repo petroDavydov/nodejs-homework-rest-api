9cf66d64040018f1c657ed5c8b89cf3013247963
petroDavydov <petrodavydov@gmail.com>
2021-12-19 05:18:02 +0200

    add get, post

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,52 @@
-const express = require('express')
-const router = express.Router()
+import { Router } from "express";
+import model from "../../model/index";
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = new Router();
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  const contacts = await model.listContacts();
+  res.status(200).json(contacts);
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const contact = await model.getContactById(id);
+  if (contact) {
+    return res.status(200).json(contact);
+  }
+  res.status(404).json({ message: "Not Found" });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
 
-module.exports = router
+
+
+
+
+router.post("/", async (req, res, next) => {
+	const newContact =await model.addContact(req.body)
+  res.status(200).json(newContact);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+router.delete("/:id", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+router.patch("/:id", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+export default router;
