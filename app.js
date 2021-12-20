187ee12e33b722c8d38535cd1bc7066617e5e845
petroDavydov <petrodavydov@gmail.com>
2021-12-20 11:35:55 +0200

    reef1 Tanja

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
-import Joi from 'joi'
-// import getRouterById from './controllers/contacts/getContactById'
-import contactsRouter from './controllers/contacts'
 
-// import contactsRouter from "./routes/api/contacts";
-
+import contactsRoutes from "./routes";
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
-app.use(express.json()); // json
-app.use(express.urlencoded({ extended: false })); // form
+app.use(express.json());
 
-app.use("/api/contacts", contactsRouter);
-// app.use("/controllers/contacts", Joi(contactsRouter));
+app.use("/controllersContacts", contactsRoutes.routerListContacts);
+app.use("/controllersContacts", contactsRoutes.routerGetContactById);
+app.use("/controllersContacts", contactsRoutes.routerPostContact);
+app.use("/controllersContacts", contactsRoutes.routerDeleteContact);
+app.use("/controllersContacts", contactsRoutes.routerPutContact);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
