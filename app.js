2615d243cedae1bd5e0d331101fcc77272e0ff20
petroDavydov <petrodavydov@gmail.com>
2021-12-30 10:38:58 +0200

    add

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import { HttpCode } from './lib/constants'
 
 import contactsRouter from './routes/api/contacts'
+import authRouter from './routes/api/auth'
 
 const app = express()
 
@@ -12,8 +13,9 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json()) // json
-app.use(express.urlencoded({ extended: false })) // forms
+// app.use(express.urlencoded({ extended: false })) // forms
 
+app.use('/api/auth', authRouter)
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
