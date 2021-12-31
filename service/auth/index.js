24d3aa14f5f7ad7f24f7a85f971eb6d7d3321552
petroDavydov <petrodavydov@gmail.com>
2021-12-31 14:45:48 +0200

    add h24

--- service/auth/index.js
+++ service/auth/index.js
@@ -30,7 +30,7 @@ class AuthService {
   getToken(user) {
     const { id, email } = user
     const payload = { id, email }
-    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' })
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '24h' })
     return token
   }
 
