2be8f620ab2d6423b594656a7fef932e06d34f3e
petroDavydov <petrodavydov@gmail.com>
2022-01-06 18:00:18 +0200

    add limit mark in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import logger from "morgan";
 import cors from "cors";
 import helmet from "helmet";
-import { HttpCode } from "./lib/constants";
+import { HttpCode,LIMIT_JSON } from "./lib/constants";
 
 import contactsRouter from "./routes/api/contacts";
 import authRouter from "./routes/api/auth";
@@ -13,7 +13,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(helmet());
 app.use(logger(formatsLogger));
 app.use(cors());
-app.use(express.json()); // json
+app.use(express.json({ limit: LIMIT_JSON })); // json
 // app.use(express.urlencoded({ extended: false })) // forms
 
 app.use("/api/auth", authRouter);
