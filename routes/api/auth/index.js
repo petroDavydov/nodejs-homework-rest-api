8d2fca9ba5439f0b87310c809563eff00ed539dd
petroDavydov <petrodavydov@gmail.com>
2022-01-06 17:57:36 +0200

    add limit for registration from one IP

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -1,11 +1,12 @@
-import { Router } from 'express'
-import { registration, login, logout } from '../../../controllers/auth'
-import guard from '../../../middlewares/guard'
+import { Router } from "express";
+import { registration, login, logout } from "../../../controllers/auth";
+import guard from "../../../middlewares/guard";
+import limiter from "../../../middlewares/rate-limit";
 
-const router = new Router()
+const router = new Router();
 
-router.post('/registration', registration)
-router.post('/login', login)
-router.post('/logout', guard, logout)
+router.post("/registration", limiter(15 * 60 * 1000, 2), registration);
+router.post("/login", login);
+router.post("/logout", guard, logout);
 
-export default router
+export default router;
