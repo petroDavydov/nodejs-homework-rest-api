89c078b87167cb1af44a75d27c5287d118630a54
petroDavydov <petrodavydov@gmail.com>
2022-01-23 22:12:03 +0200

    add createEmailTemplate

--- service/email/service.js
+++ service/email/service.js
@@ -1,6 +1,5 @@
-import { EMailService } from ".";
-
-class EMailService {
+import Mailgen from "mailgen";
+class EmailService {
   constructor(env, sender) {
     this.sender = sender;
     switch (env) {
@@ -17,6 +16,34 @@ class EMailService {
         this.link = "http://localhost:3000/";
     }
   }
+  createEmailTemplate(username, verifyToken) {
+    const mailGenerator = new Mailgen({
+      theme: "default",
+      product: {
+        name: "Group 35",
+        link: this.link,
+      },
+    });
+
+    const email = {
+      body: {
+        name: username,
+        intro: "Welcome to Mailgen! We're very excited to have you on board.",
+        action: {
+          instructions: "To get started with our API, please click here:",
+          button: {
+            color: "#22BC66", // Optional action button color
+            text: "Confirm your account",
+            link: `${this.link}/api/users/verify/${verifyToken}`,
+          },
+        },
+        outro:
+          "Need help, or have questions? Just reply to this email, we'd love to help.",
+      },
+    };
+  }
+
+  async sendVerifyEmail(email, username, verifyToken) {}
 }
 
-export default EMailService;
+export default EmailService;
