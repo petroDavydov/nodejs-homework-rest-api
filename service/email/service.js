ee68ca429bbbf1f0e2534cc3119635293fb766be
petroDavydov <petrodavydov@gmail.com>
2022-01-24 21:05:31 +0200

    sendgrid

--- service/email/service.js
+++ service/email/service.js
@@ -4,7 +4,7 @@ class EmailService {
     this.sender = sender;
     switch (env) {
       case "development":
-        this.link = "http://localhost:3000/";
+        this.link = "http://localhost:5050/";
         break;
       case "test":
         this.link = "http://localhost:5000/";
@@ -13,7 +13,7 @@ class EmailService {
         this.link = "http://heroku/";
         break;
       default:
-        this.link = "http://localhost:3000/";
+        this.link = "http://localhost:5050/";
     }
   }
   createEmailTemplate(username, verifyToken) {
@@ -34,7 +34,7 @@ class EmailService {
           button: {
             color: "#22BC66", // Optional action button color
             text: "Confirm your account",
-            link: `${this.link}/api/users/verify/${verifyToken}`,
+            link: `${this.link}api/users/verify/${verifyToken}`,
           },
         },
         outro: "If need help, write us",
@@ -45,7 +45,7 @@ class EmailService {
 
   async sendVerifyEmail(email, username, verifyToken) {
     const emailBody = this.createEmailTemplate(username, verifyToken);
-    const message = {
+    const msg = {
       to: email,
       subject: "Verify email",
       html: emailBody,
