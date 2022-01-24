1b699f65a3ee8c3e8f5f318cd39d15199ee9871b
petroDavydov <petrodavydov@gmail.com>
2022-01-24 12:06:07 +0200

    add vrify

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -7,6 +7,12 @@ import {
   LocalFileStorage,
 } from "../../service/file-storage";
 
+import {
+  EmailService,
+  SenderNodemailer,
+  SenderSendgrid,
+} from "../../service/email";
+
 const aggregation = async (req, res, next) => {
   const { id } = req.params;
   const data = await repositoryContacts.getStatisticsContacts(id);
@@ -52,14 +58,39 @@ const verifyUser = async (req, res, next) => {
 };
 
 const repeatEmailForVerifyUser = async (req, res, next) => {
-  const uploadService = new AvatarStorage(LocalFileStorage, req.file, req.user);
-  const avatarUrl = await uploadService.updateAvatar();
+  const { email } = req.body;
+  const { user } = await repositoryUsers.findByEmail(email);
+  if (user) {
+    const { email, name, verifyTokenEmail } = user;
+    const emailService = new EmailService(
+      process.env.NODE_ENV,
+      new SenderNodemailer()
+    );
 
-  res.status(HttpCode.OK).json({
-    status: "success",
-    code: HttpCode.OK,
-    message: "Success!",
-    data: { avatarUrl },
+    const isSend = await emailService.sendVerifyEmail(
+      email,
+      name,
+      verifyTokenEmail
+    );
+
+    if (isSend) {
+      return res.status(HttpCode.OK).json({
+        status: "success",
+        code: HttpCode.OK,
+        data: { message: "Success" },
+      });
+    }
+
+    res.status(HttpCode.UNPROCESSABLE_ENTITY).json({
+      status: "error",
+      code: HttpCode.UNPROCESSABLE_ENTITY,
+      data: { message: "UNPROCESSABLE ENTITY" },
+    });
+  }
+  res.status(HttpCode.NOT_FOUND).json({
+    status: "error",
+    code: HttpCode.NOT_FOUND,
+    data: { message: "User with email not found" },
   });
 };
 
--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -3,6 +3,7 @@ import {
   aggregation,
   uploadAvatar,
   verifyUser,
+  repeatEmailForVerifyUser,
 } from "../../../controllers/users";
 import guard from "../../../middlewares/guard";
 import { upload } from "../../../middlewares/upload";
@@ -14,5 +15,6 @@ const router = new Router();
 router.get("/stats/:id", guard, roleAccess(Role.ADMIN), aggregation);
 router.patch("/avatar", guard, upload.single("avatar"), uploadAvatar);
 router.get("/verify/:token", verifyUser);
+router.post("./verify", repeatEmailForVerifyUser);
 
 export default router;
